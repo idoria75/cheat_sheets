3cf496afc41f0868979b0853a1596d9a89b6a84e
Ivan Doria <ivanpdoria@gmail.com>
2022-04-13 11:18:50 +0200

    Add base_class_script

---
+++ base_class_script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+import csv
+
+# Rename class
+class BaseClass():
+    def __init__(self):
+        pass
+
+# Change class constructor based on name given to class
+if __name__ == '__main__':
+    class = BaseClass()
